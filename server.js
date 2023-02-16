cff2f45c754a237b567104950adadcbefcff4e29
dannypaton <dannypaton@gmail.com>
2023-02-15 20:21:02 -0500

    ‘w’

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://whitesands-stake-hipforcrypto.vercel.app', 'https://stake.whitesands.game', 'https://claim.whitesands.game', 'https://whitesands-stake.vercel.app', 'https://ws-claim.vercel.app', 'https://ws-claim.vercel.app/api/claim', 'https://ws-items-site.vercel.app', 'https://marketplace.whitesands.game', 'https://api.trexsapiens-order.com', 'https://bbsc-api.vercel.app', 'https://lottery-git-launchme-hipforcrypto.vercel.app'],
+    originWhitelist: ['http://localhost:3000', 'https://whitesands-stake-hipforcrypto.vercel.app', 'https://stake.whitesands.game', 'https://claim.whitesands.game', 'https://whitesands-stake.vercel.app', 'https://ws-claim.vercel.app', 'https://ws-claim.vercel.app/api/claim', 'https://ws-items-site.vercel.app', 'https://marketplace.whitesands.game', 'https://api.trexsapiens-order.com', 'https://bbsc-api.vercel.app', 'https://lottery-git-launchme-hipforcrypto.vercel.app', 'https://blazebet.io'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
